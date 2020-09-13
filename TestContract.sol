3d04d66d6ce5b055a9c2266da0dc5cd9f85302b9
JackTanRoo <jack.tan.lu@hotmail.com>
2020-09-12 20:50:25 -0700

    changed contract output to str

--- TestContract.sol
+++ TestContract.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.7.1;
 
 contract FirstContract {
-  function getInteger() public pure returns (uint) {
-    return 123;
+  function getInteger(string memory input) public pure returns (string memory) {
+    return input;
   }
 }
+
+// deploy address 0x1A19163b1d6A16E998d112Ba6E05412Bad869e2B
